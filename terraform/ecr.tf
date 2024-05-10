01e3ead7b6f97793dcf9445992166ccee551163c
Matheus Borges <matob@live.com>
2024-05-10 01:32:52 -0300

    chore: configure CI/CD

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -6,3 +6,27 @@ resource "aws_ecr_repository" "default" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "default" {
+  repository = aws_ecr_repository.default.name
+
+  policy = <<EOF
+  {
+    "rules": [
+      {
+        "rulePriority": 1,
+        "description": "Expire images older than 14 days",
+        "selection": {
+          "tagStatus": "untagged",
+          "countType": "sinceImagePushed",
+          "countUnit": "days",
+          "countNumber": 14
+        },
+        "action": {
+          "type": "expire"
+        }
+      }
+    ]
+  }
+  EOF
+}
